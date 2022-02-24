60ab5521ca0ceee41b6b60f2c8a966a59f4fcdf0
abelisle-mulesoft <abelisle@mulesoft.com>
2022-02-24 17:02:38 -0500

    Updated WSDL and implementation to handle SOAP headers

--- src/main/java/org/mulesoft/hospitality/oracle/oracleowsmock/service/AvailabilityEndpoint.java
+++ src/main/java/org/mulesoft/hospitality/oracle/oracleowsmock/service/AvailabilityEndpoint.java
@@ -4,11 +4,14 @@ import com.micros.webservices.og._4_3.availability.AvailabilityRequestType;
 import com.micros.webservices.og._4_3.availability.AvailabilityResponseType;
 import com.micros.webservices.ows._5_1.availability.AvailabilityMockInterface;
 import org.springframework.stereotype.Service;
+import com.micros.webservices.og._4_3.core.OGHeaderType;
+
+import javax.xml.ws.Holder;
 
 @Service
 public class AvailabilityEndpoint implements AvailabilityMockInterface {
     @Override
-    public AvailabilityResponseType availability(AvailabilityRequestType availabilityRequest) {
+    public AvailabilityResponseType availability(Holder<OGHeaderType> header, AvailabilityRequestType availabilityRequest) {
         GeneralAvailability generalAvailability = new GeneralAvailability();
 
         return generalAvailability.getAvailability(availabilityRequest);
